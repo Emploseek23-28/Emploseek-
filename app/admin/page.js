4885069884cb2263099ac671ebce0819b34e90c0
Emploseek23-28 <emploseekteam@gmail.com>
2025-12-27 18:52:41 +0100

    Create page.js

---
+++ app/admin/page.js
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Admin Panel</h1>
+      <p>Password: admin123</p>
+    </div>
+  )
+}
