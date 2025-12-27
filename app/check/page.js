3ebe95b78c6d4c417799c572bca2b30d2287afae
Emploseek23-28 <emploseekteam@gmail.com>
2025-12-27 18:51:42 +0100

    Create page.js

---
+++ app/check/page.js
@@ -0,0 +1,8 @@
+export default function CheckPage() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1 style={{ color: 'blue' }}>Reference Check</h1>
+      <p>Vérifiez votre contrat ici</p>
+    </div>
+  )
+}
